e27e4b36f0c45dbb5afb7454d28e078fcc277f29
ejmejm <ninjaedan@aol.com>
2019-02-14 11:49:05 -0500

    breakout example

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import cv2
 
 def reshape_train_var(var):
     var_shape = np.array(var[0]).shape
@@ -18,3 +19,17 @@ def gaussian_likelihood(x, mu, std):
     pre_sum = -(0.5*tf.log(2.*np.pi)) - (0.5*tf.log(std)) - (tf.square(x - mu))/(2.*std+1e-8)
     
     return tf.reduce_sum(pre_sum, axis=1)
+
+def to_grayscale(img):
+    return np.dot(img[:,:,:3], [0.299, 0.587, 0.114])
+
+def resize_img(img, size=(64, 64)):
+    return cv2.resize(img, dsize=size).reshape(list(size) + [1])
+
+def scale_color(img, max_val=255.):
+    return img / max_val
+
+def preprocess_atari(obs, size=(64, 64)):
+    gray = to_grayscale(obs)
+    resized = resize_img(gray, size)
+    return scale_color(resized)

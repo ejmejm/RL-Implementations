08cd7e12e3de689d9823c481e118e3bdf0c5a64f
ejmejm <ninjaedan@aol.com>
2019-02-24 12:03:15 -0500

    added ludus

---
+++ ludus/env.py
@@ -0,0 +1,116 @@
+import numpy as np
+import threading
+import time
+import gym
+from ludus.cart_pole_cont import CartPoleEnv
+from ludus.memory import MTMemoryBuffer
+
+def make_cart_pole():
+    return gym.make('CartPole-v1')
+
+def make_cart_pole_c():
+    return CartPoleEnv()
+
+def make_car_race():
+    return gym.make('CarRacing-v0')
+
+def make_lunar_lander_d():
+    return gym.make('LunarLander-v2')
+
+def make_lunar_lander_c():
+    return gym.make('LunarLanderContinuous-v2')
+
+class EnvController():
+    def __init__(self, make_env, n_threads=1, memory_buffer=MTMemoryBuffer(), 
+                 obs_transform=None, act_transform=None):
+        self.make_env = make_env
+        self.mb = memory_buffer
+        self.n_threads = n_threads
+        if obs_transform is not None:
+            self.obs_transform = obs_transform
+        if act_transform is not None:
+            self.act_transform = act_transform
+        self.act_lock = threading.Lock()
+        self.init_lock = threading.Lock()
+        
+    def obs_transform(self, obs):
+        return obs.squeeze()
+    
+    def act_transform(self, act):
+        return act
+    
+    def set_obs_transform(self, transform_func):
+        self.obs_transform = transform_func
+    
+    def set_act_transform(transform_func):
+        self.act_transform = transform_func
+        
+    def sim_thread(self, agent_id, network, n_episodes=1, max_steps=200, render=False):
+        with self.init_lock:
+            env = self.make_env()
+        
+        for episode in range(n_episodes):
+            self.mb.start_rollout(agent_id)
+            obs = env.reset()
+            obs = self.obs_transform(obs)
+            for step in range(max_steps):
+                with self.act_lock:
+                    act = network.gen_act(obs)
+                act = self.act_transform(act)
+
+                obs_next, rew, d, _ = env.step(act)
+                obs_next = self.obs_transform(obs_next)
+
+                if render:
+                    env.render()
+                    time.sleep(0.02)
+
+                self.mb.record(agent_id, obs, act, rew, obs_next)
+                obs = obs_next
+
+                if d:
+                    break
+                    
+    def sim_episodes(self, network, n_episodes=1, max_steps=200, render=False, return_data=False):
+        threads = []
+        ept = [int(n_episodes // self.n_threads) for i in range(self.n_threads)] # Episodes per thread
+        ept[:(n_episodes % self.n_threads)] += np.ones((n_episodes % self.n_threads,))
+        for i in range(self.n_threads):
+            new_thread = threading.Thread(target=self.sim_thread, args=(i, network, int(ept[i]), max_steps, render))
+            threads.append(new_thread)
+            new_thread.start()
+            
+        for thread in threads:
+            thread.join()
+        
+        if return_data:
+            return self.mb.to_data()
+        
+    def get_avg_reward(self):
+        return self.mb.get_avg_reward()
+    
+    def get_data(self):
+        return self.mb.to_data()
+    
+    def render_episodes(self, network, n_episodes=1, max_steps=200):
+        with self.init_lock:
+            env = self.make_env()
+        
+        for episode in range(n_episodes):
+            obs = env.reset()
+            obs = self.obs_transform(obs)
+            for step in range(max_steps):
+                with self.act_lock:
+                    act = network.gen_act(obs)
+                act = self.act_transform(act)
+
+                obs_next, rew, d, _ = env.step(act)
+                obs_next = self.obs_transform(obs_next)
+                obs = obs_next
+
+                env.render()
+                time.sleep(0.02)
+
+                if d:
+                    break
+        
---
+++ ludus/utils.py
@@ -0,0 +1,41 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+
+def discount_rewards(rewards, gamma=0.99):
+    new_rewards = [rewards[-1]]
+    for i in reversed(range(len(rewards)-1)):
+        new_rewards.append(rewards[i] + gamma * new_rewards[-1])
+    return new_rewards[::-1]
+
+def reshape_train_var(var):
+    var_shape = np.array(var[0]).shape
+    if var_shape == ():
+        return var
+    n_samples = len(var)
+    concated = np.concatenate(var)
+    reshaped = np.array(concated).reshape([n_samples] + list(var_shape))
+    return reshaped
+
+def gaussian_likelihood(x, mu, std):
+    """
+    Calculate the Gaussian Likelihood of a given input for using in continuous
+    action spaces.
+    """
+    pre_sum = -(0.5*tf.log(2.*np.pi)) - (0.5*tf.log(std)) - (tf.square(x - mu))/(2.*std+1e-8)
+    
+    return tf.reduce_sum(pre_sum, axis=1)
+
+def to_grayscale(img):
+    return np.dot(img[:,:,:3], [0.299, 0.587, 0.114])
+
+def resize_img(img, size=(64, 64)):
+    return cv2.resize(img, dsize=size).reshape(list(size) + [1])
+
+def scale_color(img, max_val=255.):
+    return img / max_val
+
+def preprocess_atari(obs, size=(84, 84)):
+    gray = to_grayscale(obs)
+    resized = resize_img(gray, size)
+    return scale_color(resized)
